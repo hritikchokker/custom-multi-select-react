b06d7acfd5407985c6d7b15c42b7a95eb6f7f97d
hritikchokker <hchokker7@gmail.com>
2021-04-01 17:12:49 +0530

    1 file modified, 2 added

---
+++ src/Multiselect.js
@@ -0,0 +1,31 @@
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import Multiselectoption from "./Multiselectoption";
+export default function Multiselect() {
+  const [dropDownData, setDropdownData] = useState([]);
+  const helperMultiSelect = [];
+  // {
+  //   "userId": 1,
+  //   "id": 1,
+  //   "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  //   "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  // },
+  async function fetchData() {
+    try {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1/todos"
+      );
+      const data = await res.json();
+      setDropdownData(data);
+    } catch (err) {}
+  }
+  useLayoutEffect(() => {
+    try {
+      fetchData();
+    } catch (err) {}
+  });
+  return (
+    <>
+      <Multiselectoption dropDownData={helperMultiSelect} />
+    </>
+  );
+}
---
+++ src/Multiselectoption.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+export default function Multiselectoption({ dropDownData }) {
+  const [selectedItems, setSelectedItem] = useState([]);
+  const [inputEl, setInputEl] = useState("");
+
+  const handleSelectChange = event => {
+    const { value, name } = event.target;
+    if (!selectedItems.find(el => el.toLowerCase() === value.toLowerCase())) {
+      const items = selectedItems;
+      items.push(value);
+      // items.unshift(value);
+      setSelectedItem(items);
+    }
+    // if(selected){
+
+    // }
+  };
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      const { value } = event.target;
+      if (value && value.length) {
+        if (
+          !selectedItems.find(el => el.toLowerCase() === value.toLowerCase())
+        ) {
+          const items = selectedItems;
+          items.unshift(value);
+          setSelectedItem(items);
+        } else {
+          alert("item is already added in the list");
+        }
+        setInputEl("");
+      }
+      console.log("enter press here! ", inputEl, selectedItems);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          onKeyPress={handleKeyPress}
+          onChange={event => setInputEl(event.target.value)}
+          type="text"
+        />
+      </div>
+      <label for="cars">Choose a car:</label>
+      <select onChange={handleSelectChange} name="cars" multi="true" multiple>
+        {dropDownData.forEach(element => {
+          console.log(element, "sss");
+          return <option value={element.id}>{element.title}</option>;
+        })}
+        // <option value="volvo">Volvo</option>
+        // <option value="saab">Saab</option>
+        // <option value="mercedes">Mercedes</option>
+        // <option value="audi">Audi</option>
+      </select>
+      <p> hold down ctrl(command for mac) key to select multiple item</p>
+    </>
+  );
+}
