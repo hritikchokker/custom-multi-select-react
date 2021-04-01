e80968de4f2d7577e788525e51b3fd33c9befab5
hritikchokker <hchokker7@gmail.com>
2021-04-01 18:08:44 +0530

    2 files modified

--- src/Multiselect.js
+++ src/Multiselect.js
@@ -2,7 +2,6 @@ import React, { useLayoutEffect, useState, useEffect } from "react";
 import Multiselectoption from "./Multiselectoption";
 export default function Multiselect() {
   const [dropDownData, setDropdownData] = useState([]);
-  const helperMultiSelect = [];
   // {
   //   "userId": 1,
   //   "id": 1,
@@ -10,30 +9,39 @@ export default function Multiselect() {
   //   "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
   // },
   // useEffect(() => {}, [dropDownData]);
-  async function fetchData() {
-    try {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1/todos"
-      );
-      const data = await res.json();
-      return data;
-    } catch (err) {}
-  }
+  // async function fetchData() {
+  //   try {
+  //     const res = await fetch(
+  //       "https://jsonplaceholder.typicode.com/users/1/todos"
+  //     );
+  //     const data = await res.json();
+  //     return data;
+  //   } catch (err) {}
+  // }
   useEffect(() => {
-    const res = async () => {
-      try {
-        const result = await fetchData();
-        if (result) {
-          console.log(result, "res");
-          setDropdownData(result);
-        }
-      } catch (err) {}
-    };
+    fetch("https://jsonplaceholder.typicode.com/users/1/todos")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res, "ds");
+        setDropdownData(res);
+      })
+      .catch(err => err);
+    // const res = async () => {
+    //   try {
+    //     const result = await fetchData();
+    //     if (result) {
+    //       console.log(result, "res");
+    //       setDropdownData(result);
+    //     }
+    //   } catch (err) {}
+    // };
     // res();
   }, []);
   return (
     <>
-      <Multiselectoption dropDownData={dropDownData} />
+      <Multiselectoption
+      update={((event)=>setDropdownData(event))}
+       dropDownData={dropDownData} />
     </>
   );
 }
--- src/Multiselectoption.js
+++ src/Multiselectoption.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-export default function Multiselectoption({ dropDownData }) {
+export default function Multiselectoption({ dropDownData, update }) {
   const [selectedItems, setSelectedItem] = useState([]);
+  const helperDropDown = Array.from(dropDownData);
   const [inputEl, setInputEl] = useState("");
+  useEffect(() => {}, []);
   const handleSelectChange = event => {
     const { value, name } = event.target;
     if (!selectedItems.find(el => el.toLowerCase() === value.toLowerCase())) {
@@ -19,18 +21,49 @@ export default function Multiselectoption({ dropDownData }) {
       const { value } = event.target;
       if (value && value.length) {
         if (
-          !selectedItems.find(el => el.toLowerCase() === value.toLowerCase())
+          !dropDownData.find(
+            el => el.title.toLowerCase() === value.toLowerCase()
+          )
         ) {
-          const items = selectedItems;
-          items.unshift(value);
-          setSelectedItem(items);
+          const items = dropDownData;
+          items.unshift({
+            completed: false,
+            id: Math.random(),
+            title: value,
+            userId: Math.random(),
+            value
+          });
+          setInputEl("");
+          update(items);
         } else {
           alert("item is already added in the list");
+          setInputEl("");
         }
-        setInputEl("");
       }
       console.log("enter press here! ", inputEl, selectedItems);
     }
+  };
+  const filterDropDown = value => {
+    if (!value) {
+      setInputEl("");
+    } else {
+      setInputEl(value);
+    }
+    // console.log(inputEl, "input El");
+    // if (!inputEl) {
+    //   update(helperDropDown);
+    //   return;
+    // }
+    // if (inputEl) {
+    //   update(
+    //     dropDownData.filter(el =>
+    //       el.title.toLowerCase().includes(inputEl.toLowerCase())
+    //     )
+    //   );
+    // }
+    //  else {
+    //   update(helperDropDown);
+    // }
   };
 
   return (
@@ -38,22 +71,30 @@ export default function Multiselectoption({ dropDownData }) {
       <div>
         <input
           onKeyPress={handleKeyPress}
-          onChange={event => setInputEl(event.target.value)}
+          onChange={event => filterDropDown(event.target.value)}
           type="text"
+          value={inputEl}
         />
       </div>
       <label htmlFor="cars">Choose a car:</label>
       <select onChange={handleSelectChange} name="cars" multi="true" multiple>
-        {dropDownData.forEach(element => {
-          console.log(element, "sss");
-          return <option value={element.id}>{element.title}</option>;
+        {dropDownData.map((element, index) => {
+          return (
+            <>
+              <option key={index} value={element.id}>
+                {element.title}
+              </option>
+            </>
+          );
         })}
-        // <option value="volvo">Volvo</option>
-        // <option value="saab">Saab</option>
-        // <option value="mercedes">Mercedes</option>
-        // <option value="audi">Audi</option>
+        {dropDownData.length === 0 ? <>No data found</> : <></>}
       </select>
       <p> hold down ctrl(command for mac) key to select multiple item</p>
     </>
   );
 }
+
+// <option value="volvo">Volvo</option>
+// <option value="saab">Saab</option>
+// <option value="mercedes">Mercedes</option>
+// <option value="audi">Audi</option>
